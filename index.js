be1c1fc16aeb1cbb172f33454c067e5ca5912f8f
Josue Abraham <josuea@noukod.com>
2019-02-28 15:34:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   const token = 'b2d935f1f8a0d25f1b21ee47efb82ec8ea8ff3e1';
+  return token
   //return '';
 }
 
