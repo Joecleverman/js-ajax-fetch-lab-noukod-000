1b27e9baab468db5c03df85eff40dff379353163
Josue Abraham <josuea@noukod.com>
2019-02-28 16:07:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-function getIssues() {
-}
-
-function showIssues(json) {
-}
-
-function createIssue() {
-}
-
-function showResults(json) {
-}
-
-function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
-}
-
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '';
+}
+
+function forkRepo() {
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const url = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
+  fetch(url, {
+   method: 'post',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${getToken()}`
+    }
+   })
+  .then(res => res.json())
+  .then(json => console.log(json));
+}
+
+function showResults(json) {
+  //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
+}
+
+function createIssue() {
+  //use this function to create an issue based on the values input in index.html
+    const url = 'https://api.github.com/repos/Joecleverman//javascript-fetch-lab/issues';
+    const postData = {
+      title: document.getElementById('title').value,
+      body: document.getElementById('body').value
+};
+
+ fetch(url, {
+   method: 'post',
+   body: JSON.stringify(postData),
+   headers: {
+    Authorization: `token ${getToken()}`
+  }
+  }).then(res => res.json()).then(json => getIssues());
+}
+
+function getIssues() {
+  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const url = 'https://api.github.com/repos/Joecleverman/javascript-fetch-lab/issues';
+
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
 }
